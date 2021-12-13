c9553c3ef79b6c6073905c0beabc19acd3a215f8
Tom Collyer <tomcollyer1998@gmail.com>
2021-12-13 14:50:15 +0000

    playlist variables created

--- src/main/java/com/bae/playlist/domain/Playlist.java
+++ src/main/java/com/bae/playlist/domain/Playlist.java
@@ -1,5 +1,28 @@
 package com.bae.playlist.domain;
 
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity // flags class as a table to spring data
 public class Playlist {
+
+	@Id // primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // sets auto increment for ID
+	private Integer id;
+	@Column(nullable = false)
+	private String artistName;
+	@Column(nullable = false)
+	private String songName;
+	@Column(nullable = false)
+	private String albumName;
+	@Column(nullable = false)
+	private String genre;
+	@Column(nullable = false)
+	private Date releaseDate;
 
 }
